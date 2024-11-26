130b3ed9846a1b9cbff7c1fc4d65cf3d5a944c28
Luis Lopez <luislopez@Luiss-MacBook-Pro.local>
2024-11-26 14:00:06 -0700

    troubleshoot port issues and revise function to GET all books detail

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,9 +11,12 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+  try {
+    return res.status(200).json({ books });
+  } catch (error) {
+    return res.status(500).json({ message: "Error retrieving the book list." });
+  }
 });
 
 // Get book details based on ISBN
