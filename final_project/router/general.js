fba6dcbc4350f91ffac4a451b09a9ebd21918f5c
Luis Lopez <luislopez@Luiss-MacBook-Pro.local>
2024-11-26 14:23:14 -0700

    revise function to GET books by title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -50,9 +50,21 @@ public_users.get('/author/:author', function (req, res) {
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+  const { title } = req.params;
+  const matchingBooks = [];
+
+  for (const key in books) {
+    if (books[key].title === title) {
+      matchingBooks.push(books[key]);
+    }
+  }
+
+  if (matchingBooks.length > 0) {
+    return res.status(200).json({ books: matchingBooks });
+  } else {
+    return res.status(404).json({ message: `No books found with title "${title}"` });
+  }
 });
 
 //  Get book review
