5940f0e3f301494c03f8e9eb1a4f5377977032d6
Luis Lopez <luislopez@Luiss-MacBook-Pro.local>
2024-11-26 14:07:47 -0700

    revise function to GET book details based off ISBN number

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,10 +20,16 @@ public_users.get('/', function (req, res) {
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+  const { isbn } = req.params;
+  const book = books[isbn];
+
+  if (book) {
+    return res.status(200).json({ book });
+  } else {
+    return res.status(404).json({ message: `Book with ISBN ${isbn} not found` });
+  }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
