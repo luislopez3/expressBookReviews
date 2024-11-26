048d8315e29080b9c36312a4f7dc373b2023f061
Luis Lopez <luislopez@Luiss-MacBook-Pro.local>
2024-11-26 14:28:01 -0700

    revise the function to GET the book by review

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -67,10 +67,16 @@ public_users.get('/title/:title', function (req, res) {
   }
 });
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Get book review
+public_users.get('/review/:isbn', function (req, res) {
+  const { isbn } = req.params;
+  const book = books[isbn];
+
+  if (book) {
+    return res.status(200).json({ reviews: book.reviews });
+  } else {
+    return res.status(404).json({ message: `No reviews found for book with ISBN ${isbn}` });
+  }
 });
 
 module.exports.general = public_users;
