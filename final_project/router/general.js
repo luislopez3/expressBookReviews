8f8997f559fa530929e37c445ba1dd6d6c9aca8b
Luis Lopez <luislopez@Luiss-MacBook-Pro.local>
2024-11-26 21:48:06 -0700

    revise functions to use promise callbacks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,52 +21,84 @@ public_users.post("/register", (req, res) => {
   return res.status(201).json({ message: "User registered successfully." });
 });
 
-// Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
   const { isbn } = req.params;
-  const book = books[isbn];
 
-  if (book) {
-    return res.status(200).json({ book });
-  } else {
-    return res.status(404).json({ message: `Book with ISBN ${isbn} not found` });
-  }
+  const findBookByISBN = new Promise((resolve, reject) => {
+    const book = books[isbn];
+
+    if (book) {
+      resolve(book);
+    } else {
+      reject(`Book with ISBN ${isbn} not found`);
+    }
+  });
+
+  findBookByISBN
+    .then(book => {
+      res.status(200).json({ book });
+    })
+    .catch(errorMessage => {
+      res.status(404).json({ message: errorMessage });
+    });
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   const { author } = req.params;
-  const matchingBooks = [];
 
-  for (const key in books) {
-    if (books[key].author === author) {
-      matchingBooks.push(books[key]);
+  const findBooksByAuthor = new Promise((resolve, reject) => {
+    const matchingBooks = [];
+
+    for (const key in books) {
+      if (books[key].author === author) {
+        matchingBooks.push(books[key]);
+      }
     }
-  }
 
-  if (matchingBooks.length > 0) {
-    return res.status(200).json({ books: matchingBooks });
-  } else {
-    return res.status(404).json({ message: `No books found by author ${author}` });
-  }
+    if (matchingBooks.length > 0) {
+      resolve(matchingBooks);
+    } else {
+      reject(`No books found by author ${author}`);
+    }
+  });
+
+  findBooksByAuthor
+    .then(matchingBooks => {
+      res.status(200).json({ books: matchingBooks });
+    })
+    .catch(errorMessage => {
+      res.status(404).json({ message: errorMessage });
+    });
 });
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
   const { title } = req.params;
-  const matchingBooks = [];
 
-  for (const key in books) {
-    if (books[key].title === title) {
-      matchingBooks.push(books[key]);
+  const findBooksByTitle = new Promise((resolve, reject) => {
+    const matchingBooks = [];
+
+    for (const key in books) {
+      if (books[key].title === title) {
+        matchingBooks.push(books[key]);
+      }
     }
-  }
 
-  if (matchingBooks.length > 0) {
-    return res.status(200).json({ books: matchingBooks });
-  } else {
-    return res.status(404).json({ message: `No books found with title "${title}"` });
-  }
+    if (matchingBooks.length > 0) {
+      resolve(matchingBooks);
+    } else {
+      reject(`No books found with title "${title}"`);
+    }
+  });
+
+  findBooksByTitle
+    .then(matchingBooks => {
+      res.status(200).json({ books: matchingBooks });
+    })
+    .catch(errorMessage => {
+      res.status(404).json({ message: errorMessage });
+    });
 });
 
 // Get book review
